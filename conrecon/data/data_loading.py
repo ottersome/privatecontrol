468838ac88779ba3e113b1c66061fe94aba52947
ottersome <luisgarcia26284@gmail.com>
2024-09-30 19:10:05 +0800

    Adding: Initial Batching of Data per Run.

--- conrecon/data/data_loading.py
+++ conrecon/data/data_loading.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from typing import List
+from typing import List, DefaultDict
 import pdb
 import itertools
 
@@ -15,12 +15,44 @@ import itertools
 #
 #     return data_so_far.values
 
-def new_format(path: str, features_per_run:int = 15) -> List:
+
+def df_from_run(df: pd.DataFrame, features_per_run: int) -> pd.DataFrame:
+    """
+    Will take a dataframe and go one by one all the features and create a new timeline more easily to view
+    """
+    # Drop only rows that re completely empty. Still retain those that have nans
+    df = df.dropna(axis=0, how="all")  # CHECK:
+    INDEX_NAME = "Time"
+
+    #  We need to ensure we get N/A values
+    all_features = DefaultDict(lambda: {f"f_{i}": None for i in range(features_per_run)})
+    for r in range(df.shape[0]):
+        for c in range(features_per_run):
+            time = df.iloc[r, 2 * c]
+            val = df.iloc[r, 2 * c + 1]
+            if time == ' ':
+                continue
+            all_features[time][f"f_{c}"] = val
+    # Create a new dataframe with the new features
+    df = pd.DataFrame.from_dict(all_features, orient="index")
+    df.index.name = INDEX_NAME
+
+    # Drop rows that have each columns empty (AND ACROSS COLUMNS)
+    print(f"Normally considered colums {df.columns.tolist()}")
+    df = df.dropna(axis=0, how="all", subset=df.columns.tolist())
+    df_sorted = df.sort_values(by=INDEX_NAME,  ascending=True, key=lambda col: col.astype(float))
+
+    # Write new_df to csv
+    df_sorted.to_csv("new_df.csv")
+
+
+
+def new_format(path: str, features_per_run: int = 15) -> List:
     """
     This will only be for conveting the csv to a new more amenable format
     args:
         - path: The path to the csv file
-        - features_per_run: The number of features per run. 
+        - features_per_run: The number of features per run.
     """
     # Remove first column
     data_so_far = pd.read_csv(path, skiprows=1, index_col=0, header=1)
@@ -29,28 +61,13 @@ def new_format(path: str, features_per_run:int = 15) -> List:
     print(f"Number of runs is {num_runs}")
     num_runs = int(num_runs)
 
-    # On each run we will try to align 
+    # On each run we will try to align
     for i in range(num_runs):
         # Get the data for this run
-        run_data = data_so_far.iloc[:, i * features_per_run : (i + 1) * features_per_run]
+        run_data = data_so_far.iloc[
+            :, i * (features_per_run * 2) : (i + 1) * (features_per_run * 2)
+        ]
         # Get the time steps for this run
-        indexers = [ 2*i for i in range(features_per_run)]
+        indexers = [2 * i for i in range(features_per_run)]
+        df_from_run(run_data, features_per_run)
         pdb.set_trace()
-        run_time_steps = data_so_far.iloc[:, indexers ]
-        flattened_datapoints = run_data.values
-        set_datapoints = list(set(itertools.chain.from_iterable(flattened_datapoints)))
-
-        # Create new df with set_datapoints as indexers
-        new_df = pd.DataFrame(index=set_datapoints)
-        for i in range(features_per_run):
-            new_df[i] = run_data.iloc[:, i]
-
-        flattened_datapoints = run_data.values
-        # Create a  set of time steps for this run : 
-
-        # Lets see what we have
-        print(f"Run {i} has shape {run_data.shape} and time steps {run_time_steps.shape}")
-        print(f"Run {i} has data {run_data.head()} and time steps {run_time_steps.head()}")
-        print(f"Run {i} has data {run_data.tail()} and time steps {run_time_steps.tail()}")
-
-
