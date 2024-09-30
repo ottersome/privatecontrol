1bb56463b93d8a48e7c96bb202e941ac686d0d9d
ottersome <luisgarcia26284@gmail.com>
2024-09-30 22:55:53 +0800

    Adding: Dataloading succesfully partitioning its input

--- conrecon/data/data_loading.py
+++ conrecon/data/data_loading.py
@@ -24,6 +24,8 @@ def df_from_run(df: pd.DataFrame, features_per_run: int) -> pd.DataFrame:
     df = df.dropna(axis=0, how="all")  # CHECK:
     INDEX_NAME = "Time"
 
+    print(f"Logging dataframe with head {df.head()}")
+
     #  We need to ensure we get N/A values
     all_features = DefaultDict(lambda: {f"f_{i}": None for i in range(features_per_run)})
     for r in range(df.shape[0]):
@@ -37,17 +39,21 @@ def df_from_run(df: pd.DataFrame, features_per_run: int) -> pd.DataFrame:
     df = pd.DataFrame.from_dict(all_features, orient="index")
     df.index.name = INDEX_NAME
 
+    print(
+        f"We ended up with the following stats with this dataframe:\n"
+        f"Initial time is {df.index[0]} and final time is {df.index[-1]}\n"
+        f"With a total of {df.shape[0]} rows and {df.shape[1]} columns\n"
+    )
     # Drop rows that have each columns empty (AND ACROSS COLUMNS)
-    print(f"Normally considered colums {df.columns.tolist()}")
     df = df.dropna(axis=0, how="all", subset=df.columns.tolist())
     df_sorted = df.sort_values(by=INDEX_NAME,  ascending=True, key=lambda col: col.astype(float))
 
     # Write new_df to csv
-    df_sorted.to_csv("new_df.csv")
+    return df_sorted
 
 
 
-def new_format(path: str, features_per_run: int = 15) -> List:
+def new_format(path: str, features_per_run: int = 15):
     """
     This will only be for conveting the csv to a new more amenable format
     args:
@@ -57,9 +63,10 @@ def new_format(path: str, features_per_run: int = 15) -> List:
     # Remove first column
     data_so_far = pd.read_csv(path, skiprows=1, index_col=0, header=1)
 
-    num_runs = data_so_far.shape[1] / features_per_run
+    num_runs = data_so_far.shape[1] / (features_per_run*2)
     print(f"Number of runs is {num_runs}")
     num_runs = int(num_runs)
+    og_path = "/".join(path.split("/")[:-1])
 
     # On each run we will try to align
     for i in range(num_runs):
@@ -68,6 +75,8 @@ def new_format(path: str, features_per_run: int = 15) -> List:
             :, i * (features_per_run * 2) : (i + 1) * (features_per_run * 2)
         ]
         # Get the time steps for this run
-        indexers = [2 * i for i in range(features_per_run)]
-        df_from_run(run_data, features_per_run)
-        pdb.set_trace()
+        print(f"About to enter the {i}th run. With a dataframe with {run_data.shape[1]} columns and {run_data.shape[0]} rows")
+        new_df = df_from_run(run_data, features_per_run)
+        new_df.to_csv(f"{og_path}/run_{i}.csv")
+
+    # NOTE: This is basically all I need it for so I wont bother with nicer returns and the like
