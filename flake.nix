72565408fa11a93621fd609557b6087c8505c65d
ottersome <luisgarcia26284@gmail.com>
2024-09-30 13:01:53 +0800

    Nix: Stress-free environment

--- flake.nix
+++ flake.nix
@@ -3,90 +3,71 @@
 
   inputs = { flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
-    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
-    };
   };
 
-  outputs = { self, nixpkgs, unstable, flake-utils, poetry2nix } @ inputs:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix } @ inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
         # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-        pkgs = nixpkgs.legacyPackages.${system};
-        unstable = import unstable { inherit system; };
-        poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
-        env = poetry2nix.mkPoetryEnv {
-          projectDir = ./.;
-          editablePackageSources = {
-            my-app = ./src;
-          };
-          python = pkgs.python310;
-          preferWheels = true;
-          overrides = poetry2nix.overrides.withDefaults (
-            final: prev: {
-              cramjam = prev.cramjam.override{
-                preferWheel = true;
-                nativeBuildInputs = prev.nativeBuildInputs or [ ] ++ [
-                  # pkgs.rustPlatform.cargoSetupHook # handles `importCargoLock`
-                  pkgs.rustPlatform.maturinBuildHook # cramjame is based on maturin
-                ];
-              };
-              # fsspec = prev.fsspec.overridePythonAttrs (
-              #   old: nixpkgs.lib.optionalAttrs (nixpkgs.lib.versionAtLeast old.version "7.0.0") {
-              #     buildInputs = old.buildInputs or [ ] ++ [
-              #       prev.hatchling
-              #     ];
-              #   }
-              # );
-              fsspec = prev.fsspec.override {
-                preferWheel = true;
-              };
-              scikit-base = prev.scikit-base.overridePythonAttrs(old: rec {
-                preferWheel = true;
-                postInstall = ''
-                    echo "Cleaning up __pycache__ directories..."
-                    rm -rf $out/lib/python3.10/site-packages/docs
-
-                    # Optionally call the old postInstall hook if it exists
-                    ${old.postInstall or ""}
-                '';
-              });
-
-              #  chineseize-matplotlib = prev.chineseize-matplotlib.override {
-              #   preferWheel = true;
-              # };
-              # };
-            }
-          );
-
+        # pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs { 
+          config.allowUnfree = true;
+          cudaSupport = true;
+          inherit 
+            system;
         };
 
       in
       {
-        devShells.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.git
-          pkgs.nodejs
-          env
+
+      # Thisis the only thing that can make my environment run.
+      devShells.default = (pkgs.buildFHSUserEnv {
+        name = "cuda-env";
+        targetPkgs = pkgs: with pkgs; [ 
+          git
+          gitRepo
+          gnupg
+          autoconf
+          curl
+          procps
+          gnumake
+          util-linux
+          m4
+          gperf
+          unzip
+          lazygit
+          cudatoolkit
+          linuxPackages.nvidia_x11
+          libGLU libGL
+          xorg.libXi xorg.libXmu freeglut
+          xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib 
+          ncurses5
+          stdenv.cc
+          binutils
+          poetry
+          python310
         ];
+        multiPkgs = pkgs: with pkgs; [ zlib ];
+        runScript = "zsh";
+        # If host has nvim installed, mount it to the container
+        profile = ''
+          export CUDA_PATH=${pkgs.cudatoolkit}
+          export RPROMPT="%F{cyan}( Poetry)%f"
+          # export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib
+          export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+          export EXTRA_CCFLAGS="-I/usr/include"
+          # Tell poetry to use the right python
+          # Also mount cache for nvim 
+          # Check if poetry environment must be installed for first time 
+          poetry_envs=$(poetry env list -q)
+          if [ $? -ne 0 ]; then
+            poetry env use ${pkgs.python310}/bin/python
+            poetry install --no-root
+          else # just install poetry shell
+            echo -e "\033[1;33m Remember to use \`poetry shell\` to activate the environment\033[0m"
+          fi
+          '';
+        }).env;
 
-        shellHook = ''
-          echo "Welcome to the PrivateControl Environment."
-
-          export IN_FLAKE=1
-          export SHELL=${nixpkgs.legacyPackages.x86_64-linux.zsh}/bin/zsh
-          exec ${nixpkgs.legacyPackages.x86_64-linux.zsh}/bin/zsh
-        '';
-      };
-
-        # Shell for poetry.
-        #
-        #     nix develop .#poetry
-        #
-        # Use this shell for changes to pyproject.toml and poetry.lock.
-        devShells.poetry = pkgs.mkShell {
-          packages = [ pkgs.poetry ];
-        };
       });
 }
