75f0b397049471b447f22e58868598c46c59561f
ottersome <luisgarcia26284@gmail.com>
2024-10-15 15:45:57 +0800

    Training

--- conrecon/dplearning/vae.py
+++ conrecon/dplearning/vae.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 from torch.distributions import Normal
 from conrecon.utils import create_logger
 
+
 class VAE(nn.Module):
     def __init__(self, input_size, latent_size, hidden_size):
         super(VAE, self).__init__()
@@ -40,6 +41,7 @@ class VAE(nn.Module):
         z = self.reparameterize(mu, sigma)
         return self.decode(z)
 
+
 class DP2VAE(nn.Module):
     def __init__(self, input_size, latent_size, hidden_size):
         super(DP2VAE, self).__init__()
@@ -55,6 +57,77 @@ class DP2VAE(nn.Module):
         # Start with normal Normal distribution
         self.N = Normal(torch.zeros(latent_size), torch.ones(latent_size))
         self.kl = 0
+
+
+class AdversarialVAE(nn.Module):
+    def __init__(
+        self,
+        input_size: int,
+        latent_size: int,
+        hidden_size: int,
+        num_features_to_guess: int,
+    ):
+        super(AdversarialVAE, self).__init__()
+        self.input_size = input_size
+        self.latent_size = latent_size
+        self.hidden_size = hidden_size
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc21 = nn.Linear(hidden_size, latent_size)
+        self.fc22 = nn.Linear(hidden_size, latent_size)
+        self.fc3 = nn.Linear(latent_size, hidden_size)
+        self.fc4 = nn.Linear(hidden_size, input_size)
+        self.logger = create_logger(__class__.__name__)
+
+        # For adversary
+        self.adversary = nn.Sequential(
+            nn.Linear(latent_size, latent_size*2),
+            nn.ReLU(),
+            nn.Linear(latent_size*2, num_features_to_guess),
+        )
+        self.relu = nn.ReLU()
+
+        # Start with normal Normal distribution
+        self.N = Normal(0, 1)
+        self.kl = 0
+
+    def encode(self, x):
+        # This normally expects (batch_size, sequence_length, input_size)
+        if len(x.shape) == 3:
+            reshaped_x = x.view(-1, x.shape[-1])
+        elif len(x.shape) == 2:
+            reshaped_x = x
+        else:
+            raise ValueError(f"Shape of x is {x.shape} and its type is {type(x)}")
+
+        # Keep x i
+        self.logger.debug(f"Shape of x is {x.shape} and its type is {type(x)}")
+        h1 = F.relu(self.fc1(reshaped_x))
+        return self.fc21(h1), self.fc22(h1)
+
+    def reparameterize(self, mu, sigma):
+        self.logger.debug(f"The sigma and mu shapes are ({sigma.shape},{mu.shape})")
+        sample = self.N.sample(mu.shape)
+        self.logger.debug(f"Shape of sample is {sample.shape}")
+        d0 = sigma * self.N.sample(mu.shape).to(sigma.device)
+        z = mu + d0
+        # CHECK: this to be correct.
+        # self.kl = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).sum()
+        self.kl = 0.5 * torch.sum(sigma**2 + mu**2 - torch.log(sigma**2) - 1)
+        return z
+
+    def decode(self, z):
+        h3 = F.relu(self.fc3(z))
+        return self.fc4(h3)
+
+    def forward(self, x):
+        mu, sigma = self.encode(x)
+        z = self.reparameterize(mu, sigma)
+        decoded = self.decode(z)
+        decoded = decoded.view(x.shape)
+
+        guessed_features = self.adversary(z)
+        return decoded, guessed_features
+
 
 class FlexibleVAE(nn.Module):
 
@@ -110,6 +183,7 @@ class FlexibleVAE(nn.Module):
         decoded = decoded.view(x.shape)
         return decoded
 
+
 # Recurrent Variational Autoencoder for Time Series
 class RecurrentVAE(nn.Module):
     def __init__(
@@ -142,16 +216,20 @@ class RecurrentVAE(nn.Module):
         # Pass input through LSTM encoder
         rnn_output, _ = self.lstm_encoder(x)
         self.logger.debug(f"The shapes of all the hidden bois are {rnn_output.shape}")
-        rnn_output = rnn_output.contiguous().view(rnn_output.size(0), -1)  # Flatten LSTM output
+        rnn_output = rnn_output.contiguous().view(
+            rnn_output.size(0), -1
+        )  # Flatten LSTM output
         return (
-            self.fc21(rnn_output).view(rnn_output.shape[0],rnn_output.shape[1],-1),
-            self.fc22(rnn_output).view(rnn_output.shape[0],rnn_output.shape[1],-1)
+            self.fc21(rnn_output).view(rnn_output.shape[0], rnn_output.shape[1], -1),
+            self.fc22(rnn_output).view(rnn_output.shape[0], rnn_output.shape[1], -1),
         )
 
     def reparameterize(self, mu, sigma):
         self.logger.debug(f"The sigma and mu shapes are ({sigma.shape},{mu.shape})")
         sample = self.N.sample(mu.shape)
-        self.logger.debug(f"Sigze of sigma {sigma.shape} as awell as tha tof the sample {sample.shape}")
+        self.logger.debug(
+            f"Sigze of sigma {sigma.shape} as awell as tha tof the sample {sample.shape}"
+        )
         d0 = sigma * sample
         z = mu + d0
         self.kl = 0.5 * torch.sum(sigma**2 + mu**2 - torch.log(sigma**2) - 1)
@@ -168,4 +246,3 @@ class RecurrentVAE(nn.Module):
         # We have the resulting mu and sigma for the sequence to regenerate.
         z = self.reparameterize(mu, sigma)
         return self.decode(z)
-    
