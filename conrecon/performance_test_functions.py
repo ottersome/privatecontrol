5cfcc1df0344491238955990212919ae9820dc3c
ottersome <luisgarcia26284@gmail.com>
2025-02-19 15:01:26 -0600

    Adding: ts_paretto.py

--- conrecon/performance_test_functions.py
+++ conrecon/performance_test_functions.py
@@ -292,3 +292,67 @@ def vae_test_file(
     model_adversary.train()
 
     return {k: np.mean(v).item() for k, v in metrics.items()}
+
+def get_tradeoff_metrics(
+    test_file: np.ndarray,
+    idxs_colsToGuess: Sequence[int],
+    model_vae: nn.Module,
+    model_adversary: nn.Module,
+    sequence_length: int,
+    padding_value: int,
+    batch_size: int = 16,
+) -> Tuple[float, float]:
+    """
+    Will run a validation iteration for a model
+    Args:
+        - validation_file: Validation data (file_length, num_features)
+        - model: Model to run the validation on
+        - col_to_predict: (0-index) which column we would like to predict
+    """
+    ########################################
+    # Setup some simple stuff here
+    ########################################
+    metrics = {
+        "recon_loss": [],
+        "adv_loss": [],
+    }
+    model_vae.eval()
+    model_adversary.eval()
+    device = next(model_vae.parameters()).device
+    test_x = torch.from_numpy(test_file).to(torch.float32).to(device)
+    public_columns = list(set(range(test_x.shape[-1])) - set(idxs_colsToGuess))
+    private_columns = list(idxs_colsToGuess)
+    test_pub = test_x[:,public_columns]
+    test_prv = test_x[:,private_columns]
+    num_batches = ceil(len(test_x) / batch_size)
+
+    ########################################
+    # Go through all the public data first
+    ########################################
+
+    batch_guesses = []
+    batch_reconstructions = []
+    for batch_no in range(num_batches):
+        ########################################
+        # Sanitize the data
+        ########################################
+        start_idx = batch_no * batch_size
+        end_idx = min((batch_no + 1) * batch_size, test_x.shape[0])
+        backhistory = collect_n_sequential_batches(test_x.cpu().numpy(), start_idx, end_idx, sequence_length, padding_value)
+        backhistory = torch.from_numpy(backhistory).to(torch.float32).to(device)
+        latent_z, sanitized_data, kl_divergence = model_vae(backhistory)
+
+        # TODO: Incorporate Adversary Guess
+        adversary_guess = model_adversary(latent_z)
+        batch_guesses.append(adversary_guess)
+        batch_reconstructions.append(sanitized_data)
+
+    seq_guesses = torch.cat(batch_guesses, dim=0)
+    seq_reconstructions = torch.cat(batch_reconstructions, dim=0)
+
+
+    # Now we create our average metrics
+    recon_mse = torch.mean((seq_reconstructions - test_pub) ** 2)
+    adv_mse = torch.mean((seq_guesses - test_prv) ** 2)
+
+    return  adv_mse.item(),recon_mse.item()
---
+++ ts_paretto.py
@@ -0,0 +1,167 @@
+import debugpy
+import numpy as np
+import torch
+import wandb
+import argparse
+
+import matplotlib.pyplot as plt
+
+from conrecon.data.data_loading import load_defacto_data, split_defacto_runs
+from conrecon.dplearning.adversaries import Adversary
+from conrecon.dplearning.vae import SequenceToScalarVAE
+from conrecon.utils import create_logger, set_seeds
+from conrecon.performance_test_functions import get_tradeoff_metrics
+from conrecon.training_utils import train_vae_and_adversary
+
+def argsies() -> argparse.Namespace:
+    ap = argparse.ArgumentParser()
+    # State stuff here
+    ap.add_argument(
+        "-e", "--epochs", default=50, help="How many epochs to train for", type=int
+    )
+    ap.add_argument("-l", "--lr", default=0.01, type=float) 
+    ap.add_argument(
+        "--defacto_data_raw_path",
+        default="./data/",
+        type=str,
+        help="Where to load the data from",
+    )
+    ap.add_argument("--batch_size", default=16, type=int)
+    ap.add_argument("--rnn_num_layers", default=2, type=int)
+    ap.add_argument("--rnn_hidden_size", default=15, type=int)
+    ap.add_argument(
+        "--cols_to_hide",
+        default=[4],
+        help="Which are the columsn we want no information of",
+    )  # Remember 0-index (so 5th)
+    ap.add_argument("--vae_latent_size", default=32, type=int)
+    ap.add_argument("--episode_length", default=32, type=int)
+    ap.add_argument("--episode_gap", default=3, type=int)
+    ap.add_argument("--vae_hidden_size", default=32, type=int)
+    ap.add_argument(
+        "--splits",
+        default={"train_split": 0.8, "val_split": 0.2, "test_split": 0.0},
+        type=list,
+        nargs="+",
+    )
+    ap.add_argument("--kl_dig_hypr", "-k", default=0.1, type=float) # type: ignore 
+    ap.add_argument("--seed", default=0, type=int) 
+    ap.add_argument("--debug", "-d", action="store_true", help="Whether or not to use debugpy for trainig") 
+    ap.add_argument("--debug_port", default=42020, help="Port to use for debugging") 
+    ap.add_argument("--wandb", "-w", action="store_true", help="Whether or not to use wandb for logging") 
+    ap.add_argument("--padding_value", default=-1, type=int) 
+
+    return ap.parse_args()
+
+def main():
+    args = argsies()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    set_seeds(args.seed)
+    logger = create_logger("main_training")
+
+    if args.debug:
+        logger.info("\033[1;33m Waiting for debugger to attach...\033[0m")
+        debugpy.listen(("0.0.0.0", args.debug_port))
+        debugpy.wait_for_client()
+
+    # TODO: Make it  so that generate_dataset checks if params are the same
+    columns, runs_dict, debug_file = load_defacto_data(args.defacto_data_raw_path)
+    num_columns = len(columns)
+    num_private_cols = len(args.cols_to_hide)
+    num_public_cols = num_columns - num_private_cols
+
+    # Separate them into their splits (and also interpolate)
+    train_seqs, val_seqs, test_file = split_defacto_runs(
+        runs_dict,
+        args.splits["train_split"],
+        args.splits["val_split"],
+        args.episode_length,
+        True, # Scale
+    )
+
+    ########################################
+    # Hyperparemters
+    ########################################
+    # TODO: vary this boi
+    # utility_vs_privacy_tradeoff = np.linspace(0.0, 10, 10) 
+    # Do logspace instead
+    utility_vs_privacy_tradeoff = np.logspace(-3, 3, 11, base=2)
+
+    ########################################
+    # Setup up the models
+    ########################################
+    vae_input_size = len(columns) # I think sending all of them is better
+    # TODO: Get the model going
+
+    logger.debug(f"Columns are {columns}")
+    logger.debug(f"Runs dict is {runs_dict}")
+
+    privacies = []
+    utilities = []
+    for uvp in utility_vs_privacy_tradeoff:
+        ########################################
+        # Training VAE and Adversary
+        ########################################
+        model_vae = SequenceToScalarVAE(
+            input_size=vae_input_size,
+            num_santiitzed_features=num_public_cols,
+            latent_size=args.vae_latent_size,
+            hidden_size=args.vae_hidden_size,
+            num_features_to_guess=1,
+            rnn_num_layers=args.rnn_num_layers,
+            rnn_hidden_size=args.rnn_hidden_size,
+        ).to(device)
+        model_adversary = Adversary(
+            input_size=args.vae_latent_size,
+            hidden_size=args.vae_hidden_size,
+            num_classes=num_private_cols,
+        ).to(device)
+
+        logger.info("Starting the VAE Training")
+        model_vae, model_adversary, recon_losses, adv_losses = train_vae_and_adversary(
+            args.batch_size,
+            args.cols_to_hide,
+            columns,
+            device,
+            train_seqs,
+            val_seqs,
+            args.epochs,
+            model_vae, # type: ignore
+            model_adversary, # type: ignore 
+            args.lr,
+            args.kl_dig_hypr,
+            args.wandb,
+            logger,
+            uvp
+        )
+        privacy, utility = get_tradeoff_metrics(
+            test_file,
+            args.cols_to_hide,
+            model_vae,
+            model_adversary,
+            args.episode_length,
+            args.padding_value,
+            args.batch_size,
+        )
+        privacies.append(privacy)
+        utilities.append(utility)
+        logger.info(f"Validation Metrics are {privacy}, {utility}")
+
+    # Now we plot all of the results
+    plt.figure(figsize=(16,10))
+    plt.scatter(privacies, utilities)
+    for i, uvp in enumerate(utility_vs_privacy_tradeoff): 
+        plt.annotate(f"UVP: {uvp:.2f}", (privacies[i], utilities[i]), xytext=(5, 5), textcoords='offset points')
+    plt.title("Privacy vs Utility")
+    plt.xlabel("Privacy") 
+    plt.ylabel("Utility")                           
+    # plt.xscale('log')
+    plt.savefig(f"./figures/privacy_vs_utility.png")
+    plt.close() 
+
+    logger.info("All baselines complete. Exiting")
+    exit()
+
+if __name__ == "__main__":
+    logger = create_logger("main_vae")
+    main()  
