eb0b848866381adef9f42953699d43f86fb4d849
ottersome <luisgarcia26284@gmail.com>
2025-03-05 11:19:05 -0600

    Moving PCA benchmarking to its own file

--- main_ts.py
+++ main_ts.py
@@ -298,107 +298,6 @@ def baseline_pca_decorrelation(
 
     return adversary, retained_components
 
-
-def pca_decomposition_w_heatmap(
-    ds_train: OrderedDict[str, np.ndarray],
-    test_file: np.ndarray,
-    prv_features_idxs: List[int],
-    batch_size: int,
-    correlation_threshold: float,
-    epochs: int,
-    lr: float,
-    device: torch.device,
-):
-    """
-    This will create a matrix M_{up} that will help us convert public components into private components.
-    """
-    pca = PCA()
-
-    first_key = list(ds_train.keys())[0]
-    num_features = ds_train[first_key].shape[-1]
-    pub_features_idxs = list(set(range(num_features)) - set(prv_features_idxs))
-    sequence_length = ds_train[first_key].shape[1]
-
-    # We need to concatenate the features as usual 
-    all_train_seqs = np.concatenate([ seqs for _, seqs in ds_train.items()], axis=0)
-    # Shuffle, Batch, Torch Coversion, Feature Separation
-    
-    train_pub = all_train_seqs[:,:,pub_features_idxs]
-    train_prv = all_train_seqs[:,:,prv_features_idxs]
-    train_pub_flat = train_pub.reshape(-1, train_pub.shape[-1])
-    train_prv_flat = train_prv.reshape(-1, train_prv.shape[-1])
-    train_pub_centered = train_pub_flat - np.mean(train_pub_flat, axis=0)
-    train_pub_mean = np.mean(train_pub_flat, axis=0)
-
-    #  Now we can start fitting the pca 
-    pca_transform = pca.fit(train_pub_centered)
-    # Shape is (num_components, num_features), fyi for indexing purposes
-    principal_components = pca_transform.components_
-    # Takes in (num_componets, num_features) and returns (num_components, num_samples)
-    C = train_pub_centered.dot(principal_components.T)
-
-    # The projection matrix from public to latent space
-    M_CP = pca.components_.T  # shape: (M, K)
-
-    # ----- Step 2: Solve for M_CU via linear regression -----
-    # We assume X_U \approx C @ M_CU
-    # M_CU has shape (K, N-M)
-    # Using np.linalg.lstsq to solve: M_CU = argmin ||C @ M - X_U||^2
-    M_CU, residuals, rank, s = np.linalg.lstsq(C, train_prv_flat, rcond=None)
-
-    # ----- (Optional) Directly check (C^T C) invertibility -----
-    # If C^T C is singular or near-singular, direct inversion is problematic
-    CtC = C.T @ C
-    # Attempt inversion (for demonstration; might fail if singular)
-    try:
-        CtC_inv = np.linalg.inv(CtC)
-        print("C^T C was invertible.")
-    except np.linalg.LinAlgError:
-        print("C^T C is singular or not well-conditioned.")
-
-    # ----- Step 3: Compute the final M_PU = M_CP @ M_CU -----
-    M_PU = M_CP @ M_CU  # shape: (M, N-M)
-
-    print("M_CP shape:", M_CP.shape)
-    print("M_CU shape:", M_CU.shape)
-    print("M_PU shape:", M_PU.shape)
-
-    ########################################
-    # Run Evaluation on test set
-    ########################################
-    # Now for validation
-    test_pub = test_file[:,pub_features_idxs]
-    test_prv = test_file[:,prv_features_idxs]
-    test_pub_centered = test_pub # - train_pub_mean
-    private_guess = test_pub_centered.dot(M_PU)
-
-    # Now we plot the results
-    plt.figure(figsize=(16,10))
-    plt.plot(private_guess.squeeze(), label="Reconstruction")
-    plt.plot(test_prv.squeeze(), label="Truth")
-    plt.title("PCA Reconstruction vs Truth")
-    plt.legend()
-    plt.savefig(f"./figures/mcu_reconstruction.png")
-    plt.close()
-
-
-    # We will now pass it through MPU
-
-    ########################################
-    # Plot the HeatMap
-    ########################################
-    fig, axs = plt.subplots(1, 1, figsize=(16,4))
-    im = axs.matshow(M_PU.T, cmap='viridis')
-    axs.set_title("$M_{PU}$: Public to Private Features HeatMap")
-    axs.set_xlabel("Public Features")
-    axs.set_ylabel("Private Features")
-    axs.set_yticks([])
-    plt.colorbar(im)
-    plt.savefig(f"./figures/pca_heatmap.png")
-    plt.close()
-
-
-
 def baseline_trivial_correlation(
     ds_train: OrderedDict[str, np.ndarray],
     ds_val: OrderedDict[str, np.ndarray],
@@ -420,7 +319,6 @@ def baseline_trivial_correlation(
     # Shuffle, Batch, Torch Coversion, Feature Separation
     np.random.shuffle(all_train_seqs)
     np.random.shuffle(all_valid_seqs)
-    batch_amnt  = all_train_seqs.shape[0] // batch_size
     all_train_seqs = torch.from_numpy(all_train_seqs).to(torch.float32).to(device)
     all_valid_seqs = torch.from_numpy(all_valid_seqs).to(torch.float32).to(device)
     train_pub = all_train_seqs[:,:,pub_features_idxs]
@@ -440,7 +338,6 @@ def baseline_trivial_correlation(
     adv_losses = []
     for e in tqdm(range(epochs), desc="Epochs"):
         for batch_no in tqdm(range(num_batches), desc="Batches"):
-            batch_all = all_train_seqs[batch_no * batch_size : (batch_no + 1) * batch_size]
             batch_pub = train_pub[batch_no * batch_size : (batch_no + 1) * batch_size]
             batch_prv = train_prv[batch_no * batch_size : (batch_no + 1) * batch_size]
             if batch_pub.shape[0] != batch_size:
@@ -605,37 +502,6 @@ def main():
         args.batch_size,
         wandb_on=args.wandb
     )
-
-    ########################################
-    # Training the PCA based baseline
-    ########################################
-    # TOREM: Move this lower down for when we are done with it. 
-    logger.info("Starting the PCA decorrelation and training")
-    pca_model_adversary, retained_components = baseline_pca_decorrelation(
-        train_seqs,
-        val_seqs,
-        args.cols_to_hide,
-        args.batch_size,
-        args.correlation_threshold,
-        args.epochs,
-        args.lr,
-        device,
-    )
-    logger.info("PCA training and decorrelation complete. Now testing...")
-    pca_test_entire_file(
-        test_file,
-        args.cols_to_hide,
-        pca_model_adversary,
-        retained_components,
-        args.episode_length,
-        args.padding_value,
-        logger,
-        args.batch_size,
-        wandb_on=args.wandb
-    )
-    logger.info("Done with the PCA test")
-
-    logger.info("All baselines complete. Exiting")
     exit()
 
 if __name__ == "__main__":
