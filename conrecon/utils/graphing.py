f2e7467b5a411ecd704d503301aa9d3415ec86b3
ottersome <luisgarcia26284@gmail.com>
2025-04-03 17:45:14 -0600

    Adding: Upwards Paretto Frontier

--- conrecon/utils/graphing.py
+++ conrecon/utils/graphing.py
@@ -178,7 +178,7 @@ def plot_uvps(
             f"Mismatch in {i}th_utilities vs {i}th_privacies vs {i}th_uvp_points function shape: {len(ith_utilities)} and {len(ith_privacies)} and {len(uvp_points)}"
         num_data_points = len(ith_utilities)
 
-        left_hull_x, left_hull_y = paretto_frontier(np.array(ith_privacies), np.array(ith_utilities))
+        left_hull_x, left_hull_y = paretto_frontier_up(np.array(ith_privacies), np.array(ith_utilities))
 
         ax.scatter(ith_privacies, ith_utilities, 
                             color=potential_colors_dot_colors[i],
@@ -223,8 +223,62 @@ def plot_uvps(
              loc='best')
     plt.savefig(save_dest)
 
+def paretto_frontier_up(
+    x: np.ndarray, y: np.ndarray
+) -> tuple[np.ndarray, np.ndarray]:
 
-def paretto_frontier(
+    assert len(x.shape) == 1, "x must be a 1d array"
+    assert len(y.shape) == 1, "y must be a 1d array"
+    assert x.shape == y.shape, "x and y must be the same shape"
+    assert x.shape[0] >= 1, "x must have more than one element"
+
+    # Sort the y direction
+    xy = np.vstack((x, y)).transpose()
+    sorted_idxs = (-xy[:, 1]).argsort() # Sorting by privacy (vertical axis)
+    xy = xy[sorted_idxs]
+
+    y_down = np.array([0, -1])
+    down_angle_rad = np.arctan2(y_down[1], y_down[0])
+
+    not_fixed = False # For sake of entering the loop
+    cp = np.copy(xy)
+    while not not_fixed:
+        not_fixed = True; # Sake of initialization
+        ccp = np.copy(cp)
+        del_idxs = []
+        pidx = 0
+        while pidx < (ccp.shape[0]-2):
+            diff12 = ccp[pidx+1] - ccp[pidx]
+            point12_angle = np.arctan2(diff12[1], diff12[0]) - down_angle_rad
+            diff23 = ccp[pidx+2] - ccp[pidx+1]
+            point23_angle = np.arctan2(diff23[1], diff23[0]) - down_angle_rad
+            if point23_angle > point12_angle:
+                not_fixed = False
+                del_idxs.append(pidx+1)
+                pidx += 2
+            else:
+                pidx += 1
+
+        # Round of deletions
+        cp = np.delete(cp, del_idxs, axis=0)
+
+    # Remove  Excess points
+    x_max_idx = np.argmax(cp[:, 0])
+    y_max_idx = np.argmax(cp[:, 1])
+
+    excess_down = np.where(cp[:, 1] < cp[x_max_idx, 1])[0]
+    excess_left = np.where(cp[:, 0] < cp[y_max_idx, 0])[0]
+
+    # Remove excess points
+    if len(excess_down) > 0:
+        cp = np.delete(cp, excess_down, axis=0)
+    if len(excess_left) > 0:
+        cp = np.delete(cp, excess_left, axis=0)
+
+    return cp[:, 0], cp[:,1]
+
+
+def paretto_frontier_down(
     x: np.ndarray, y: np.ndarray
 ) -> tuple[np.ndarray, np.ndarray]:
 
